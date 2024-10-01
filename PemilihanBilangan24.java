04ae62aa082647eb2b0482f5ffc82b72b600728f
Sharren <sharrenepf@gmail.com>
2024-10-01 10:56:24 +0700

    pemilahan bilangan

---
+++ PemilihanBilangan24.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PemilihanBilangan24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // Membuat scanner untuk menerima input
+
+        System.out.print("Masukkan bilangan: ");
+        int bilangan = input.nextInt(); // Membaca input bilangan dari user
+
+        // Struktur if-else untuk menentukan apakah bilangan genap atau ganjil
+        if (bilangan % 2 == 0) { // Mengecek jika bilangan habis dibagi 2
+            System.out.println("Angka " + bilangan + " adalah bilangan genap");
+        } else {
+            System.out.println("Angka " + bilangan + " adalah bilangan ganjil");
+        }
+    }
+}
